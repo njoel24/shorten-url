f1d720223286e4c3df4a24cd89fbcaf85d0ddb92
Cristiano Salvatore Anselmi <cristiano.anselmi@gmail.com>
2021-10-16 18:43:08 +0200

    chore: cleanup

--- src/components/Body/Short/Short.tsx
+++ src/components/Body/Short/Short.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Input from './Input/Input';
-import LastResearches from './LastResearches/LastResearches';
+import LastResearches from './LastResearches/LastResearches.lazy';
 import styles from './Short.module.css';
 import { ShortProvider } from './Short.provider';
 
